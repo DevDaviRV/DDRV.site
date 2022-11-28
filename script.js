dc7dad3ac0f257105ba8d5300c0c8be5396ecc43
Davi <104698942+DevDaviRV@users.noreply.github.com>
2022-11-28 01:28:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,14 +21,15 @@ new TypeIt("#testjs", {
 }).go();
 
 
-// carrousel techs
 
+
+// carrousel techs
 const slider = document.querySelector(".items");
 		const slides = document.querySelectorAll(".item");
 		const button = document.querySelectorAll(".button4");
 
 		let current = 0;
-		let prev = 4;
+		let prev = 5;
 		let next = 1;
 
 		for (let i = 0; i < button.length; i++) {
@@ -37,7 +38,7 @@ const slider = document.querySelector(".items");
 
 		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
-		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+		const gotoNext = () => current < 5 ? gotoNum(current + 1) : gotoNum(0);
 
 		const gotoNum = number => {
 			current = number;
@@ -50,7 +51,7 @@ const slider = document.querySelector(".items");
 				slides[i].classList.remove("next");
 			}
 
-			if (next == 5) {
+			if (next == 6) {
 				next = 0;
 			}
 
@@ -62,6 +63,117 @@ const slider = document.querySelector(".items");
 			slides[prev].classList.add("prev");
 			slides[next].classList.add("next");
 		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  //Teste efeito digitaçao sem API
+
+                              // new TypeIt("#testjs", { 
+            //     lifeLike: true, 
+            //     speed: 50,
+            //     waitUntilVisible: true
+            // })
+            //     .type("S")
+            //     .pause(0)
+            //     .type("e")
+            //     .pause(0)
+            //     .type("r")
+            //     .pause(0)
+            //     .type("v")
+            //     .pause(0)
+            //     .type("i")
+            //     .pause(0)
+            //     .type("ç")
+            //     .pause(0)
+            //     .type("o")
+            //     .pause(0)
+            //     .type("s")
+            //     .pause(0)
+            //     .type(" ")
+            //     .pause(0)
+            //     .type("q")
+            //     .pause(0)
+            //     .type("u")
+            //     .pause(0)
+            //     .type("e")
+            //     .pause(0)
+            //     .type(" ")
+            //     .pause(0)
+            //     .type("o")
+            //     .pause(0)
+            //     .type("f")
+            //     .pause(0)
+            //     .type("e")
+            //     .pause(0)
+            //     .type("r")
+            //     .pause(0)
+            //     .type("e")
+            //     .pause(0)
+            //     .type("ç")
+            //     .pause(0)
+            //     .type("o")
+            //     .pause(0)
+            //     .type(":")
+            //     .go();
+
+
+
+
+
 
 
 
