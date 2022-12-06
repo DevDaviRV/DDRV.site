e396b1982f3106c2d7472f8850fd62d08f3a2b49
Davi <104698942+DevDaviRV@users.noreply.github.com>
2022-12-06 07:35:04 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,14 +1,31 @@
-const preLoad = document.getElementsByClassName('preload-box')[0]
-const content = document.getElementsByClassName('contentP')[0]
-function loading() {
-    content.style.display = 'none'
-    preLoad.style.display = 'block';
 
-    setTimeout(() => {
-        preLoad.style.display ='none';
-        content.style.display = 'block'
-    }, 1);
+//pre load atualiza baseado no tempo de carregamento da pagina
+
+const main = document.querySelector(".preload-box");
+
+window.addEventListener("load", function () {
+    main.style.display = "none";
     
-} 
+
+})
+
+
+
+
+
+//antigo preload baseado em tempo setado em codigo
+
+// const preLoad = document.getElementsByClassName('preload-box')[0]
+// const content = document.getElementsByClassName('contentP')[0]
+// function loading() {
+//     content.style.display = 'none'
+//     preLoad.style.display = 'block';
+
+//     setTimeout(() => {
+//         preLoad.style.display ='none';
+//         content.style.display = 'block'
+//     }, 1);
+    
+// } 
 
 
