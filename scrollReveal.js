27e81d7734d31b7b02fbcb0855d0b299703a4ffd
Davi <104698942+DevDaviRV@users.noreply.github.com>
2023-01-03 14:49:06 -0300

    Add files via upload

---
+++ scrollReveal.js
@@ -0,0 +1,74 @@
+// Scroll Reveal
+if(window.innerWidth < 768){
+    ScrollReveal({ mobile: false });
+}
+
+//menu Nav
+
+ScrollReveal().reveal('.nav1', { delay: 25 }, { viewFactor: 0 });
+ScrollReveal().reveal('.nav2', { delay: 75 });
+ScrollReveal().reveal('.nav3', { delay: 125 });
+ScrollReveal().reveal('.nav4', { delay: 200 });
+ScrollReveal().reveal('.nav5', { delay: 275 });
+
+// Hero
+
+ScrollReveal({distance: '10rem'});
+ScrollReveal().reveal('#headline-h1' , {origin: 'left'});
+ScrollReveal().reveal('.headline-p', { origin: 'left' });
+ScrollReveal().reveal('.headbtn', { origin: 'left' });
+
+// Sobremim
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal({distance: '0rem'});
+ScrollReveal().reveal('#sec2-h2', {delay: 100});
+ScrollReveal().reveal('.sec2-p', {delay: 100});
+ScrollReveal().reveal('.i1', {delay: 20});
+ScrollReveal().reveal('.i2', {delay: 40});
+ScrollReveal().reveal('.i3', {delay: 80});
+ScrollReveal().reveal('.i4', {delay: 100});
+
+
+
+
+// Serviços
+
+ScrollReveal().reveal('#sec4-h2', { delay: 200 });
+ScrollReveal({distance: '2rem'});
+ScrollReveal().reveal('#testjs', { delay: 200 });
+ScrollReveal().reveal('#bgtext', { delay: 300 });
+ScrollReveal().reveal('.card2', { delay: 300 });
+ScrollReveal().reveal('.container-btn', { delay: 300 });
+
+
+
+
+
+// Projetos 
+
+ScrollReveal().reveal('#sec3-h2', { delay: 200 });
+ScrollReveal().reveal('#sec3-h3', { delay: 300 });
+ScrollReveal().reveal('#projetos', { delay: 300 });
+ScrollReveal().reveal('#linkGit', { delay: 300 });
+
+
+
+
+// FAQ
+
+ScrollReveal().reveal('.title-table1', { delay: 200 });
+ScrollReveal().reveal('.title-table2', { delay: 400 });
+ScrollReveal({distance: '4rem'});
+ScrollReveal().reveal('.faq1', { delay: 100 });
+ScrollReveal().reveal('.faq2', { delay: 200 });
+ScrollReveal().reveal('.faq3', { delay: 300 });
+ScrollReveal().reveal('.faq4', { delay: 400 });
+
+
+//footer
+
+ScrollReveal({distance: '3rem'});
+ScrollReveal({origin: 'left'});
+ScrollReveal().reveal('.left', { delay: 400 });
+ScrollReveal({origin: 'right'});
+ScrollReveal().reveal('.right', { delay: 400 });
